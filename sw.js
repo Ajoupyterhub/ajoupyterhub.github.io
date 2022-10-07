0308b22423d58d8d7097c4a8b4ab5742f3385736
Jae Young Choi <mobilechoi@ajou.ac.kr>
2022-10-07 15:17:15 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c2cc922d293deb75ebed.js"
+    "url": "webpack-runtime-c87c89b14617e168481b.js"
   },
   {
     "url": "framework-91479aec45331d6bbe1c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8d6a3f5c1a4d7487edc3e14056852c57"
+    "revision": "dd812b1c1db0a2e91d0fa5d8767e11e0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-14562c9dae80a38aebd9.js"
